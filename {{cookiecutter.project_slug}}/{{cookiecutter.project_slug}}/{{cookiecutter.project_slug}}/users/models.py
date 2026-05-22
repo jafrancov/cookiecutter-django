fdf16691bed68ad3292ca320c3f91473640ac52a
Alejandro Franco <jafrancov@gmail.com>
2026-05-22 16:46:10 -0600

    Upgrade CC-DJ 260522 Fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -2,7 +2,7 @@ import uuid as uuid_lib
 {%- if cookiecutter.username_type == "email" %}
 from typing import ClassVar
 
-{% endif -%}
+{%- endif %}
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 {%- if cookiecutter.username_type == "email" %}
